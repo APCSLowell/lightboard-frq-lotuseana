de068e0e6b8a3cf2d752ab1c32ca85a5022007d5
lotuseana <143060902+lotuseana@users.noreply.github.com>
2024-04-24 09:20:43 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,7 +10,16 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
+    lights = new boolean[numRows][numCols];
+    for (int i =0; i<numRows; i++){
+      for (int j =0; j<numCols; j++){
+        if (Math.random()<0.4){
+          lights[i][j]=true;
+        } else{
+          lights[i][j]=false;
+        }
+      }
+    }
 
   }
 
@@ -20,7 +29,19 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
+    int onInCol =0;
+    for (int i =0; i<lights.length;i++){
+      if (lights[i][col]){
+        onInCol++;
+      }
+    }
+    if(lights[row][col] && onInCol%2==0){
+      return false;
+    }
+    if(!lights[row][col]&& onInCol%3==0){
+      return true;
+    }
+    return lights[row][col];
    
  
   }
